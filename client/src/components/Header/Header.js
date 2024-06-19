6ce01d1688645437d86a81da6897de37f3135797
Nick Doucette <nickdoucette24@gmail.com>
2024-06-19 00:20:36 -0400

    Finished structure and some functionality of the logged out version of the header. Header is also responsive.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,29 +1,66 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import "./Header.scss";
+import SideMenu from "../SideMenu/SideMenu";
 import pitstopLogo from "../../assets/images/logos/pitstop-logo.png";
+import menuIcon from "../../assets/images/icons/menu-icon.svg";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      handleClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <div className="header-wrapper">
-        <nav className="nav-container">
-          <img
-            className="nav-container__logo"
-            src={pitstopLogo}
-            alt="pistop main logo with subtext 'formula 1 live'"
-          />
-          <div className="nav-bar">
-            <div className="nav-bar__weekend" href>
-              Race Weekend
+    <>
+      <header className="header">
+        <div className="header-wrapper">
+          <nav className="nav-container">
+            <img
+              className="nav-container__logo"
+              src={pitstopLogo}
+              alt="pistop main logo with subtext 'formula 1 live'"
+            />
+            <div className="nav-bar">
+              <Link className="nav-bar__link" href>
+                Race Weekend
+              </Link>
+              <Link className="nav-bar__link">This Year</Link>
             </div>
-            <div className="nav-bar__season">This Year</div>
+          </nav>
+          <div className="auth-container">
+            <button className="auth-container__button">Login</button>
+            <button className="auth-container__button">Sign Up</button>
           </div>
-        </nav>
-        <div className="auth-container">
-          <div className="auth-container__login">Login</div>
-          <div className="auth-container__signup">Sign Up</div>
+          <img
+            className="nav-container__menu"
+            onClick={handleOpen}
+            src={menuIcon}
+            alt="mobile side menu icon with 3 horizontal bars"
+          />
         </div>
-      </div>
-    </header>
+      </header>
+      <SideMenu isOpen={isOpen} handleClose={handleClose} />
+    </>
   );
 };
 
