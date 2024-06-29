b9ee3cbfd474e953aa37d2f4618ba653469bae3b
Nick Doucette <nickdoucette24@gmail.com>
2024-06-29 17:21:39 -0400

    FormErrorMessage component complete and working. On to the login form.

--- client/src/components/FormErrorMessage/FormErrorMessage.js
+++ client/src/components/FormErrorMessage/FormErrorMessage.js
@@ -1,7 +1,14 @@
+import react from "react";
+import errorIcon from "../../assets/images/icons/error-icon.svg";
 import "./FormErrorMessage.scss";
 
-const FormErrorMessage = () => {
-  return <div>FormErrorMessage</div>;
+const FormErrorMessage = ({ message }) => {
+  return (
+    <div className="error-message">
+      <img className="error-message__icon" src={errorIcon} />
+      <p className="error-message__text">{message}</p>
+    </div>
+  );
 };
 
 export default FormErrorMessage;
