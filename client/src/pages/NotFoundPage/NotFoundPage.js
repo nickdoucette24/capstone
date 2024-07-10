2075b26c8ed29bf4bc238e0bab00465670ab0fab
Nick Doucette <nickdoucette24@gmail.com>
2024-07-09 23:37:30 -0400

    NotFoundPage done.

--- client/src/pages/NotFoundPage/NotFoundPage.js
+++ client/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,7 +1,18 @@
 import "./NotFoundPage.scss";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="not-found">
+      <div className="not-found__container">
+        <h1 className="not-found__error">404</h1>
+        <h2 className="not-found__heading">Page Not Found</h2>
+        <h3 className="not-found__description">
+          The page you are looking for either does not exist or has not left the
+          garage. Please check the URL or try again later.
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
